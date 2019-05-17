8509e0bc54722074b22cd4bbae26fa87dd03f2ef
M91406 <andreas.reiter@microchip.com>
2019-05-17 11:32:04 +0200

    Updated .gitignore to exclude object files from tracking

--- p33SMPS_mcal.X/build/__P33SMPS_CH_MSTR__/production/plibs/dsPIC33C/p33SMPS_irq.o.d
+++
@@ -1,4 +0,0 @@
-build/__P33SMPS_CH_MSTR__/production/plibs/dsPIC33C/p33SMPS_irq.o:  \
- plibs/dsPIC33C/p33SMPS_irq.c  \
-plibs/dsPIC33C/p33SMPS_irq.h  \
- plibs/dsPIC33C/../p33SMPS_devices.h 
--- p33SMPS_mcal.X/build/__P33SMPS_CH_MSTR__/production/plibs/dsPIC33C/p33SMPS_mailboxes.o.d
+++
@@ -1,4 +0,0 @@
-build/__P33SMPS_CH_MSTR__/production/plibs/dsPIC33C/p33SMPS_mailboxes.o:  \
- plibs/dsPIC33C/p33SMPS_mailboxes.c  \
-plibs/dsPIC33C/p33SMPS_mailboxes.h  \
- plibs/dsPIC33C/../p33SMPS_devices.h 
